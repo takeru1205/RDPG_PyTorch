ad395bd4f518b908534d13dfc89b538e54be724a
takeru1205 <takeru0639418@gmail.com>
2021-04-19 21:18:41 +0900

    [add] add episode to replaybuffer

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
+from collections import deque
 import gym
+
+
+class ReplayBuffer:
+    def __init__(self, max_size=100):
+        self.buffer = deque(maxlen=max_size)
+
+    def add(self, episode):
+        self.buffer.append(episode)
 
 
 env = gym.make('Pendulum-v0')
 
+buffer = ReplayBuffer()
+
 for e in range(5):
     cumulative_reward = 0
     obs = env.reset()
+    obs_seq = [obs]
+    action_seq = []
+    reward_seq = []
+    info_seq = []
     for t in range(env.spec.max_episode_steps):
         action = env.action_space.sample()
 
         new_obs, reward, info, _ = env.step(action)
+        obs_seq.append(new_obs)
+        action_seq.append(action)
+        reward_seq.append(reward)
+        info_seq.append(info)
 
         cumulative_reward += reward
 
     print(cumulative_reward)
+    buffer.add([obs_seq, action_seq, reward_seq, info_seq])
+
 
 
