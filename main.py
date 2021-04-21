e1710b2eb97d9d32b7ce91209bc893383d6f9e44
takeru1205 <takeru0639418@gmail.com>
2021-04-21 16:08:41 +0900

    [update] separate initial act sequence from train loop

--- main.py
+++ main.py
@@ -8,7 +8,31 @@ from agent import RDPG
 
 env = gym.make('Pendulum-v0')
 
-agent = RDPG(env, initial_act=10)
+agent = RDPG(env)
+
+# Initial Act
+print('Initial Act Sequence Start')
+for e in range(100):
+    obs = env.reset()
+    action = env.action_space.sample() / env.action_space.high[0]
+    obs_seq = torch.zeros((1, env.spec.max_episode_steps+1, 3))
+    obs_seq[:, 0, :] = torch.tensor(obs)
+    action_seq = [action]
+    reward_seq = []
+    info_seq = []
+    for t in range(env.spec.max_episode_steps):
+        action = env.action_space.sample() / env.action_space.high[0]
+
+        new_obs, reward, info, _ = env.step(action * env.action_space.high[0])
+        obs_seq[:, t+1, :] = torch.tensor(new_obs)
+        action_seq.append(action)
+        reward_seq.append(reward)
+        info_seq.append(1 - info)
+
+        obs = new_obs
+
+    agent.store_episode([obs_seq, action_seq, reward_seq, info_seq])
+
 
 # Train
 print('Train Sequence Start')
@@ -38,8 +62,6 @@ for e in range(300):
     print(cumulative_reward)
     agent.store_episode([obs_seq, action_seq, reward_seq, info_seq])
 
-    if e < 10:
-        continue
     agent.update()
 
 # Test
@@ -48,24 +70,15 @@ for e in range(5):
     cumulative_reward = 0
     obs = env.reset()
     action = env.action_space.sample() / env.action_space.high[0]
-    obs_seq = torch.zeros((1, env.spec.max_episode_steps+1, 3))
-    obs_seq[:, 0, :] = torch.tensor(obs)
-    action_seq = [action]
-    reward_seq = []
-    info_seq = []
     hidden = (torch.randn(1, 1, 3),
               torch.randn(1, 1, 3))
     for t in range(env.spec.max_episode_steps):
         action, hidden = agent.get_action(obs, action, hidden)
 
         new_obs, reward, info, _ = env.step(action * env.action_space.high[0])
-        obs_seq[:, t+1, :] = torch.tensor(new_obs)
-        action_seq.append(action)
-        reward_seq.append(reward)
-        info_seq.append(info)
 
         obs = new_obs
         cumulative_reward += reward
 
     print(cumulative_reward)
-
+env.close()
