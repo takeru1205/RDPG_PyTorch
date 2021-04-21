caa67ce74fa8f84cd6885fa69b2377b0759c155c
takeru1205 <takeru0639418@gmail.com>
2021-04-21 15:49:29 +0900

    [fix] include action to history

--- main.py
+++ main.py
@@ -8,28 +8,29 @@ from agent import RDPG
 
 env = gym.make('Pendulum-v0')
 
-agent = RDPG(env)
+agent = RDPG(env, initial_act=10)
 
 # Train
 print('Train Sequence Start')
 for e in range(300):
     cumulative_reward = 0
     obs = env.reset()
+    action = env.action_space.sample() / env.action_space.high[0]
     obs_seq = torch.zeros((1, env.spec.max_episode_steps+1, 3))
     obs_seq[:, 0, :] = torch.tensor(obs)
-    action_seq = []
+    action_seq = [action]
     reward_seq = []
     info_seq = []
     hidden = (torch.randn(1, 1, 3),
               torch.randn(1, 1, 3))
     for t in range(env.spec.max_episode_steps):
-        action, hidden = agent.get_action(obs, hidden, e)
+        action, hidden = agent.get_action(obs, action, hidden, e)
 
         new_obs, reward, info, _ = env.step(action * env.action_space.high[0])
         obs_seq[:, t+1, :] = torch.tensor(new_obs)
         action_seq.append(action)
         reward_seq.append(reward)
-        info_seq.append(info)
+        info_seq.append(1 - info)
 
         obs = new_obs
         cumulative_reward += reward
@@ -37,6 +38,8 @@ for e in range(300):
     print(cumulative_reward)
     agent.store_episode([obs_seq, action_seq, reward_seq, info_seq])
 
+    if e < 10:
+        continue
     agent.update()
 
 # Test
@@ -44,15 +47,16 @@ print('Test Sequence Start')
 for e in range(5):
     cumulative_reward = 0
     obs = env.reset()
+    action = env.action_space.sample() / env.action_space.high[0]
     obs_seq = torch.zeros((1, env.spec.max_episode_steps+1, 3))
     obs_seq[:, 0, :] = torch.tensor(obs)
-    action_seq = []
+    action_seq = [action]
     reward_seq = []
     info_seq = []
     hidden = (torch.randn(1, 1, 3),
               torch.randn(1, 1, 3))
     for t in range(env.spec.max_episode_steps):
-        action, hidden = agent.get_action(obs, hidden)
+        action, hidden = agent.get_action(obs, action, hidden)
 
         new_obs, reward, info, _ = env.step(action * env.action_space.high[0])
         obs_seq[:, t+1, :] = torch.tensor(new_obs)
