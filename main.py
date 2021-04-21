e77fce9b29252f849415a421c74bbd044c62f63a
takeru1205 <takeru0639418@gmail.com>
2021-04-21 19:26:57 +0900

    [add] tensorboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gym
 import torch
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
 from model import Actor
 from agent import RDPG
@@ -8,7 +9,8 @@ from agent import RDPG
 
 env = gym.make('Pendulum-v0')
 
-agent = RDPG(env)
+writer = SummaryWriter(log_dir="./logs")
+agent = RDPG(env, writer=writer)
 
 # Initial Act
 print('Initial Act Sequence Start')
@@ -21,7 +23,8 @@ for e in range(30):
     reward_seq = []
     info_seq = []
     for t in range(env.spec.max_episode_steps):
-        action = env.action_space.sample() / env.action_space.high[0]
+        # action = env.action_space.sample() / env.action_space.high[0]
+        action = np.clip(env.action_space.sample(), -1, -1)
 
         new_obs, reward, info, _ = env.step(action * env.action_space.high[0])
         obs_seq[:, t+1, :] = torch.tensor(new_obs)
@@ -46,15 +49,15 @@ for e in range(300):
     action_seq = [action]
     reward_seq = []
     info_seq = []
-    hidden = (torch.randn(1, 1, 3),
-              torch.randn(1, 1, 3))
+    hidden = (torch.randn(1, 1, 64),
+              torch.randn(1, 1, 64))
     for t in range(env.spec.max_episode_steps):
         action, hidden = agent.get_action(obs, action, hidden, e, train=True)
 
         assert -1. <= action <= 1., f'Get {action}'
 
         new_obs, reward, info, _ = env.step(action * env.action_space.high[0])
-        env.render()
+        # env.render()
         obs_seq[:, t+1, :] = torch.tensor(new_obs)
         action_seq.append(action)
         reward_seq.append(reward)
@@ -64,9 +67,10 @@ for e in range(300):
         cumulative_reward += reward
 
     print(f'Episode: {e:>3}, Reward: {cumulative_reward:>8.2f}, Avearage Action{np.array(action_seq).mean():>9.5f}')
+    writer.add_scalar("Train/Reward", cumulative_reward, e)
     agent.store_episode([obs_seq, action_seq, reward_seq, info_seq])
 
-    agent.update()
+    agent.update(e)
 
 # Test
 print('Test Sequence Start')
@@ -84,5 +88,5 @@ for e in range(5):
         obs = new_obs
         cumulative_reward += reward
 
-    print(cumulative_reward)
+    print(f'Test Episode: {e:>3}, Reward: {cumulative_reward:>8.2f}')
 env.close()
