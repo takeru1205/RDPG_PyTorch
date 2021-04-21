5190ba09290bc58e0a103aab87ec95bbcc10831b
takeru1205 <takeru0639418@gmail.com>
2021-04-21 18:25:07 +0900

    [update] two fully connected layers

--- model.py
+++ model.py
@@ -10,15 +10,17 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.lstm = nn.LSTM(input_size=obs_dim+action_dim, hidden_size=hidden_size, batch_first=True)
         self.fc1 = nn.Linear(hidden_size, 128)
-        self.fc2 = nn.Linear(128, action_dim)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, action_dim)
 
-        self.fc2.weight.data.uniform_(-init_w, init_w)
-        self.fc2.bias.data.uniform_(-init_w, init_w)
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+        self.fc3.bias.data.uniform_(-init_w, init_w)
 
     def forward(self, history, hidden):
         x, hidden = self.lstm(history)
         x = F.relu(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
+        x = F.relu(self.fc2(x))
+        x = torch.tanh(self.fc3(x))
         return x, hidden
 
 class Crtic(nn.Module):
@@ -28,14 +30,16 @@ class Crtic(nn.Module):
                             hidden_size=hidden_size,
                             batch_first=True)
         self.fc1 = nn.Linear(hidden_size, 128)
-        self.fc2 = nn.Linear(128, 1)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 1)
 
-        self.fc2.weight.data.uniform_(-init_w, init_w)
-        self.fc2.bias.data.uniform_(-init_w, init_w)
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+        self.fc3.bias.data.uniform_(-init_w, init_w)
 
     def forward(self, history, hidden):
         x, hidden = self.lstm(history, hidden)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x, hidden
 
