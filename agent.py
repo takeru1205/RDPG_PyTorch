6bf098c8c2a9c808269368594890e458cbb3457d
takeru1205 <takeru0639418@gmail.com>
2021-04-21 18:24:39 +0900

    [add] reward scale

--- agent.py
+++ agent.py
@@ -8,12 +8,13 @@ from model import Actor, Crtic
 
 
 class RDPG:
-    def __init__(self, env, initial_act=30, gamma=0.98, tau=0.001, actor_lr=1e-4, critic_lr=1e-3, buffer_size=100):
+    def __init__(self, env, initial_act=30, gamma=0.98, tau=0.001, actor_lr=1e-4, critic_lr=1e-3, reward_scale=1., buffer_size=100):
 
         self.env = env
         self.gamma = gamma
         self.tau = tau
         self.initial_act = initial_act
+        self.reward_scale = reward_scale
 
         self.obs_dim = env.observation_space.shape[0]
         self.action_dim = env.action_space.shape[0]
@@ -34,12 +35,12 @@ class RDPG:
     def store_episode(self, episode):
         self.buffer.add(episode)
 
-    def get_action(self, obs, action, hidden_in, train=False):
+    def get_action(self, obs, action, hidden_in, epoch, train=False):
         history = torch.cat([torch.FloatTensor(obs), torch.FloatTensor(action)]).to(torch.float).reshape(1, 1, self.obs_dim+self.action_dim)
         action, hidden_out = self.actor(history, hidden_in)
         if not train:
             return action[0, 0].detach().numpy(), hidden_out
-        action = action[0, 0].detach().numpy() + np.random.normal(0, 0.3)
+        action = action[0, 0].detach().numpy() + np.random.normal(0, 0.1)
         return np.clip(action, -1, 1), hidden_out
 
     def soft_update(self, target_net, net):
@@ -75,7 +76,7 @@ class RDPG:
         with torch.no_grad():
             target_action, _ = self.target_actor(torch.cat([next_obs_tensor, next_action_tensor], dim=2), hidden)  # Shape(batch_size, episode_length, 1)
             target_q, _ = self.target_critic(torch.cat([next_obs_tensor, target_action], dim=2), hidden)  # Shape(batch_size, episode_length, 1)
-            y = reward_tensor + done_tensor * self.gamma * target_q  # Shape(batch_size, episode_length, 1)
+            y = reward_tensor * self.reward_scale + done_tensor * self.gamma * target_q  # Shape(batch_size, episode_length, 1)
 
         q_values, _ = self.critic(torch.cat([obs_tensor, action_tensor], dim=2), hidden)
         critic_loss = self.criterion(q_values, y)
@@ -85,7 +86,7 @@ class RDPG:
         self.critic_optimizer.step()
 
         action, _ = self.actor(torch.cat([obs_tensor, action_tensor], dim=2), hidden)
-        actor_loss = self.critic(torch.cat([obs_tensor, action], dim=2), hidden)[0].mean()
+        actor_loss = -self.critic(torch.cat([obs_tensor, action], dim=2), hidden)[0].mean()
 
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
