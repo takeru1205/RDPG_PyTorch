32f3d9a1fbf594b4ebe5117ff253a1e1566d1d7d
takeru1205 <takeru0639418@gmail.com>
2021-04-21 15:49:42 +0900

    [fix] include action to history

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ init_w = 3e-3
 class Actor(nn.Module):
     def __init__(self, obs_dim, action_dim, hidden_size=64):
         super(Actor, self).__init__()
-        self.lstm = nn.LSTM(input_size=obs_dim, hidden_size=hidden_size, batch_first=True)
+        self.lstm = nn.LSTM(input_size=obs_dim+action_dim, hidden_size=hidden_size, batch_first=True)
         self.fc1 = nn.Linear(hidden_size, 128)
         self.fc2 = nn.Linear(128, action_dim)
 
